6c6fc3be065bc25bb7a35381e66f50a323c4efc5
Alex Rudy <alex.rudy@gmail.com>
2016-02-23 09:30:46 -0800

    Improvements to the periodogram view.

--- telemetry/views/periodogram.py
+++ telemetry/views/periodogram.py
@@ -9,18 +9,24 @@ from ..models.periodogram import frequencies
 def show_periodogram(ax, periodogram, rate=1.0, **kwargs):
     """Show a periodogram on an axis."""
     rate = u.Quantity(rate, u.Hz)
+    
+    lim = np.max(rate).value / 2.0
+    
     kwargs.setdefault('alpha', 0.1)
     kwargs.setdefault('color', 'b')
     length = periodogram.shape[0]
     lines = ax.plot(frequencies(length, rate).value, periodogram, **kwargs)
     ax.set_xlabel("Frequency ({})".format(rate.unit))
+    ax.set_xscale("symlog")
     ax.set_yscale("log")
+    ax.set_xlim(-lim, lim)
     return lines
     
-def show_model_parameters(ax, model):
+def show_model_parameters(ax, model, name="Model", pos=(0.1, 0.1)):
     """Show model parameters."""
-    text = "gain = {:.2f}\ntau = {:.4f}s\nbleed = {:.2f}".format(model.gain.value, model.tau.value, model.integrator.value)
-    ax.text(0.1, 0.1, text, va='bottom', ha='left', transform=ax.transAxes)
+    text = "{:s}\n gain = {:.2f}\ntau = {:.4f}s\nbleed = {:.3f}".format(name, model.gain.value, model.tau.value, model.integrator.value)
+    x, y = pos
+    ax.text(x, y, text, va='bottom', ha='left', transform=ax.transAxes, bbox=dict(fc='white', ec='black'))
     
     
     
