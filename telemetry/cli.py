9a7ad58282b56e70938457c5ea6be2fc3426969e
Alex Rudy <alex.rudy@gmail.com>
2016-04-27 16:39:12 -0700

    CLI parser

--- telemetry/cli.py
+++ telemetry/cli.py
@@ -3,6 +3,7 @@
 Basic command line tools.
 """
 import argparse
+import datetime
 from telemetry.models import Dataset, TelemetryKind
 from sqlalchemy.sql import between
 
@@ -17,7 +18,8 @@ def parser(setup, **kwargs):
     """Make an argument parser"""
     parser = argparse.ArgumentParser(**kwargs)
     parser.add_argument("-f", "--force", help="Force this operation to complete.", action='store_true')
-    setup(parser)
+    if setup is not None:
+        setup(parser)
     
     group = parser.add_argument_group("query filtering")
     group.add_argument("-d", "--date", type=lambda s : datetime.datetime.strptime(s, "%Y-%m-%d"), help="Limit query to a single date.")
@@ -31,7 +33,7 @@ def parser(setup, **kwargs):
     Session = connect()
     opt.session = Session()
     
-    query = opt.session.query(Dataset)
+    query = opt.session.query(Dataset.id)
     if opt.date:
         query = add_date_filter(opt.date, opt.days, query)
     opt.query = query
