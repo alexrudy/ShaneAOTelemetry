75289d58758e0115d6676e21979ee6372fd868f4
Alex Rudy <alex.rudy@gmail.com>
2016-04-27 16:37:34 -0700

    Update periodogram view.

--- telemetry/views/periodogram.py
+++ telemetry/views/periodogram.py
@@ -6,17 +6,29 @@ import numpy as np
 
 from ..models.periodogram import frequencies
 
+def show_transferfunction(ax, tf, rate=1.0, **kwargs):
+    """Similar to show_periodogram but with slightly different defaults.
+    """
+    lines = show_periodogram(ax, tf, rate=1.0, **kwargs)
+    ax.set_ylabel("Error Transfer Function")
+    return lines
+
 def show_periodogram(ax, periodogram, rate=1.0, **kwargs):
     """Show a periodogram on an axis."""
-    rate = u.Quantity(rate, u.Hz)
     
+    periodogram = np.asarray(periodogram)
+    rate = u.Quantity(rate, u.Hz)
     lim = np.max(rate).value / 2.0
     
-    kwargs.setdefault('alpha', 0.1)
+    if periodogram.ndim != 1:
+        kwargs.setdefault('alpha', 0.1)
     kwargs.setdefault('color', 'b')
+    
     length = periodogram.shape[0]
-    lines = ax.plot(frequencies(length, rate).value, periodogram, **kwargs)
+    freq = frequencies(length, rate)
+    lines = ax.plot(np.asarray(freq), periodogram, **kwargs)
     ax.set_xlabel("Frequency ({})".format(rate.unit))
+    ax.set_ylabel("Power")
     ax.set_xscale("symlog")
     ax.set_yscale("log")
     ax.set_xlim(-lim, lim)
@@ -24,9 +36,11 @@ def show_periodogram(ax, periodogram, rate=1.0, **kwargs):
     
 def show_model_parameters(ax, model, name="Model", pos=(0.1, 0.1)):
     """Show model parameters."""
-    text = "{:s}\n gain = {:.2f}\ntau = {:.4f}s\nbleed = {:.3f}".format(name, model.gain.value, model.tau.value, model.integrator.value)
+    text = r"{:s}\n$\mathrm{{gain}} = {:.4f}$\n$\tau ={:.4f}\mathrm{{s}}$\n$\mathrm{{bleed}} = {:.3f}$".format(name, model.gain.value, model.tau.value, model.integrator)
+    t = text.split(r"\n")
+    print("\n".join(t))
     x, y = pos
-    ax.text(x, y, text, va='bottom', ha='left', transform=ax.transAxes, bbox=dict(fc='white', ec='black'))
+    ax.text(x, y, "\n".join(t), va='bottom', ha='left', transform=ax.transAxes, bbox=dict(fc='white', ec='black'))
     
     
     
