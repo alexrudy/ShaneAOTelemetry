e7a792aedce6cb221bf943452e926daeb9f7dbc9
Alex Rudy <alex.rudy@gmail.com>
2016-05-27 11:54:36 -0700

    Settings update

--- telemetry/default_config.py
+++ telemetry/default_config.py
@@ -7,7 +7,12 @@ CELERY_IMPORTS = ['telemetry.tasks',
 CELERY_RESULT_BACKEND = "rpc://"
 CELERY_DISABLE_RATE_LIMITS = True
 CELERY_ROUTES = {'telemetry.views.timeseries.make_movie': {'queue': 'movies'}}
+
 CELERYD_MAX_TASKS_PER_CHILD = 20
+CELERYD_POOL_RESTARTS = True
 
 TELEMETRY_ENTRYPOINTS = ['fourieranalysis = telemetry.ext.fourieranalysis:setup', 'shaneao = telemetry.ext.shaneao:setup']
+
 SQLALCHEMY_ECHO = False
+
+SHANEAO_TELEMETRY_HOST = 'user@real.ucolick.org'
