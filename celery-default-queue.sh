87c2b26006f7887d4a3f16e81c0b6f78b4b61ed1
Alex Rudy <alex.rudy@gmail.com>
2016-09-15 02:05:53 -0700

    Better names in the celery default queue

--- celery-default-queue.sh
+++ celery-default-queue.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 if [ $# -gt 0 ]; then
     echo "stopping"
-    celery -A telemetry.celery multi stop default --pidfile=celery/run/%n.pid
+    celery -A telemetry.celery multi stop 2 --pidfile=celery/run/%n.pid
 else
     echo "restarting"
-    celery -A telemetry.celery multi restart default -linfo --pidfile=celery/run/%n.pid --autoreload --logfile=celery/log/%n.log --concurrency=2
+    celery -A telemetry.celery multi restart 2 -linfo --pidfile=celery/run/%n.pid --autoreload --logfile=celery/log/%n.log -c 2
 fi
