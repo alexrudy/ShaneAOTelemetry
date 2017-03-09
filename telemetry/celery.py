2a17b4429857a1ab7aee367ad9e67e99fc244a85
Alex Rudy <alex.rudy@gmail.com>
2017-03-09 13:48:18 -0800

    Celery commands

--- telemetry/celery.py
+++ telemetry/celery.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import
 from .application import app
 from .cli import cli
 import click
+import os
+import subprocess
 celery = app.celery
 del app
 
@@ -22,7 +24,7 @@ def is_available():
     
 @cli.group(name='celery')
 def cgroup():
-    """docstring for cgroup"""
+    """control the celery instance."""
     pass
 
 @cgroup.command()
@@ -47,3 +49,40 @@ def check():
             click.echo("  pid: {pid:d}".format(**v))
             click.echo("  processes: {:s}".format(",".join("{:d}".format(p) for p in v['pool']['processes'])))
         return 0
+    
+@cgroup.command()
+@click.option("-n", default=2, type=int, help="Number of workers.")
+def start(n):
+    """start the celery queues"""
+    #     celery -A telemetry.celery multi restart 2 -linfo --pidfile=celery/run/%n.pid --logfile=celery/log/%n.log -c 2
+    from .application import app
+    
+    celery_path = os.path.expanduser(os.path.join(app.config['CELERY_DIRECTORY_ROOT'], 'celery'))
+    subprocess.check_call(['celery', '-A', __name__, 'multi', 'restart', '{:d}'.format(n),
+                           '-linfo', '--pidfile={path:s}/run/%n.pid'.format(path=celery_path), 
+                           '--logfile={path:s}/log/%n.log'.format(path=celery_path), '-c', '2'])
+
+@cgroup.command()
+@click.option("-n", default=1, type=int, help="Number of workers.")
+def start_movie(n):
+    """docstring for start_movie"""
+    # celery -A telemetry.celery worker -lINFO --concurrency=1 -n movies.%h -Q movies
+    from .application import app
+    
+    celery_path = os.path.expanduser(os.path.join(app.config['CELERY_DIRECTORY_ROOT'], 'celery'))
+    subprocess.check_call(['celery', '-A', __name__, 'worker', '--concurrency={:d}'.format(n),
+                           '-linfo', '-n', 'movies.%h', '-Q', 'movies',
+                           '--pidfile={path:s}/run/%n.pid'.format(path=celery_path), 
+                           '--logfile={path:s}/log/%n.log'.format(path=celery_path)])
+
+@cgroup.command()
+@click.option("-n", default=2, type=int, help="Number of workers.")
+def stop(n):
+    """Stop the celery queues"""
+    from .application import app
+    
+    # celery -A telemetry.celery multi stop 2 --pidfile=celery/run/%n.pid
+    celery_path = os.path.expanduser(os.path.join(app.config['CELERY_DIRECTORY_ROOT'], 'celery'))
+    subprocess.check_call(['celery', '-A', __name__, 'multi', 'stop', '{:d}'.format(n),
+                           '--pidfile={path:s}/run/%n.pid'.format(path=celery_path)])
+                    
